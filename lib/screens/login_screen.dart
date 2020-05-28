c554e8eb0882452346b00556fb155f8640120eb6
Niclas <niclas.joswig@helsinki.fi>
2020-05-28 10:28:50 +0200

    Loading Indicator while checking if user already logged in

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -25,12 +25,14 @@ class _LoginPageState extends State<LoginScreen> {
   AppUser user;
   var init = true;
   var subscription;
+  bool showLoadingIndicator = true;
 
   @override
   void initState() {
     super.initState();
     subscription = _auth.onAuthStateChanged.listen((data) {
       //TODO show loading icon until this is loaded
+
       if (data != null) {
         Provider.of<Analysen>(context, listen: false)
             .loadWithId(data.uid, false);
@@ -43,6 +45,10 @@ class _LoginPageState extends State<LoginScreen> {
           ),
         );
       } else {
+        setState(() {
+          showLoadingIndicator = false;
+        });
+
         subscription.cancel();
       }
     });
@@ -106,139 +112,143 @@ class _LoginPageState extends State<LoginScreen> {
         decoration: BoxDecoration(
             image: DecorationImage(
                 fit: BoxFit.fill, image: AssetImage("assets/bg-01.jpg"))),
-        child: Row(
-          children: <Widget>[
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 5,
-              child: Container(
-                padding: MediaQuery.of(context).size.width > 1000
-                    ? EdgeInsets.all(80)
-                    : EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Flexible(
-                      child: Container(
-                        child: Center(
-                          child: AutoSizeText(
-                            "Wilkommen zu SK!LOG \n",
-                            maxLines: 3,
-                            style: TextStyle(
-                              fontSize: 42,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
+        child: showLoadingIndicator
+            ? Center(child: CircularProgressIndicator())
+            : Row(
+                children: <Widget>[
+                  Flexible(
+                    fit: FlexFit.tight,
+                    flex: 5,
+                    child: Container(
+                      padding: MediaQuery.of(context).size.width > 1000
+                          ? EdgeInsets.all(80)
+                          : EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Flexible(
+                            child: Container(
+                              child: Center(
+                                child: AutoSizeText(
+                                  "Wilkommen zu SK!LOG \n",
+                                  maxLines: 3,
+                                  style: TextStyle(
+                                    fontSize: 42,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: AutoSizeText(
-                          "Dem Dokumentationstool für den",
-                          maxLines: 3,
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                          Flexible(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: AutoSizeText(
+                                "Dem Dokumentationstool für den",
+                                maxLines: 3,
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: AutoSizeText(
-                          "Langfristigen Erfolg im Trading.",
-                          maxLines: 3,
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                          Flexible(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: AutoSizeText(
+                                "Langfristigen Erfolg im Trading.",
+                                maxLines: 3,
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                    // SizedBox(height: 20,),
-                    Flexible(
-                      flex: 2,
-                      child: Center(
-                        child: Logo(),
-                      ),
-                    ),
+                          // SizedBox(height: 20,),
+                          Flexible(
+                            flex: 2,
+                            child: Center(
+                              child: Logo(),
+                            ),
+                          ),
 
-                    SizedBox(
-                      height: 20,
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Flexible(
+                            child: buildLink("Tritt der Community bei", () {
+                              _launchURL("https://t.me/SKTRoom");
+                            }),
+                          ),
+                          Flexible(
+                            child:
+                                buildLink("Beantrage einen SK!Log Account", () {
+                              _launchURL("https://t.me/xlDownxl");
+                            }),
+                          ),
+                          Flexible(
+                            child: buildLink("Technische Fragen", () {
+                              _launchURL("https://t.me/xlDownxl");
+                            }),
+                          ),
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
                     ),
-                    Flexible(
-                      child: buildLink("Tritt der Community bei", () {
-                        _launchURL("https://t.me/SKTRoom");
-                      }),
+                  ),
+                  Flexible(
+                    fit: FlexFit.tight,
+                    flex: 6,
+                    child: FlutterLogin(
+                      //TODO email und passwort validator in flutter_login wieder aktivieren
+                      title: '',
+                      theme: LoginTheme(
+                        accentColor: Colors.transparent,
+                        pageColorLight: Colors.transparent,
+                        pageColorDark: Colors.transparent,
+                      ),
+                      messages: LoginMessages(
+                          signupButton: "Registrieren",
+                          forgotPasswordButton: "Passwort vergessen?",
+                          passwordHint: "Passwort",
+                          goBackButton: "Zurück",
+                          recoverPasswordIntro:
+                              "Setze dein Passwort hier zurück",
+                          recoverPasswordDescription:
+                              "Du erhälst eine Email zum zurücksetzen deines Passwortes.",
+                          recoverPasswordButton: "Zurücksetzen",
+                          confirmPasswordHint: "Passwort bestätigen"),
+                      onLogin: (data) {
+                        return user.login(
+                            data,
+                            Provider.of<Analysen>(context, listen: false),
+                            Provider.of<UserTags>(context, listen: false));
+                      },
+                      onSignup: (data) {
+                        return user.register(
+                            data,
+                            Provider.of<Analysen>(context, listen: false),
+                            Provider.of<UserTags>(context, listen: false));
+                      },
+                      onRecoverPassword: user.recoverPassword,
+                      onSubmitAnimationCompleted: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (ctx) => HomeScreen(),
+                          ),
+                        );
+                      },
                     ),
-                    Flexible(
-                      child: buildLink("Beantrage einen SK!Log Account", () {
-                        _launchURL("https://t.me/xlDownxl");
-                      }),
-                    ),
-                    Flexible(
-                      child: buildLink("Technische Fragen", () {
-                        _launchURL("https://t.me/xlDownxl");
-                      }),
-                    ),
-                  ],
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                ),
+                  ),
+                ],
               ),
-            ),
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 6,
-              child: FlutterLogin(
-                //TODO email und passwort validator in flutter_login wieder aktivieren
-                title: '',
-                theme: LoginTheme(
-                  accentColor: Colors.transparent,
-                  pageColorLight: Colors.transparent,
-                  pageColorDark: Colors.transparent,
-                ),
-                messages: LoginMessages(
-                    signupButton: "Registrieren",
-                    forgotPasswordButton: "Passwort vergessen?",
-                    passwordHint: "Passwort",
-                    goBackButton: "Zurück",
-                    recoverPasswordIntro: "Setze dein Passwort hier zurück",
-                    recoverPasswordDescription:
-                        "Du erhälst eine Email zum zurücksetzen deines Passwortes.",
-                    recoverPasswordButton: "Zurücksetzen",
-                    confirmPasswordHint: "Passwort bestätigen"),
-                onLogin: (data) {
-                  return user.login(
-                      data,
-                      Provider.of<Analysen>(context, listen: false),
-                      Provider.of<UserTags>(context, listen: false));
-                },
-                onSignup: (data) {
-                  return user.register(
-                      data,
-                      Provider.of<Analysen>(context, listen: false),
-                      Provider.of<UserTags>(context, listen: false));
-                },
-                onRecoverPassword: user.recoverPassword,
-                onSubmitAnimationCompleted: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (ctx) => HomeScreen(),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
