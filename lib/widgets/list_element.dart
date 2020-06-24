e310319e78f001a8645905da23b96e0fd8dd2ad1
xlDownxl <niclas.joswig@helsinki.fi>
2020-06-24 11:45:57 +0200

    preview function

--- lib/widgets/list_element.dart
+++ lib/widgets/list_element.dart
@@ -5,10 +5,14 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'dart:math';
 import 'package:provider/provider.dart';
 import '../models/analysen.dart';
+import 'package:flutter/gestures.dart';
 class ListElement extends StatelessWidget {
   final String analyseId;
+
   ListElement(this.analyseId);
+
   double fSize = 20;
+  OverlayEntry overlayEntry;
 
   String writeTags(tags) {
     StringBuffer builder = StringBuffer();
@@ -17,76 +21,129 @@ class ListElement extends StatelessWidget {
       builder.write(", ");
     }
     var result = builder.toString();
-    return result.substring(0, max(result.length - 1,0));
+    return result.substring(0, max(result.length - 1, 0));
   }
 
+  showOverlay(BuildContext context, Analyse analyse, Offset position) async {
+    OverlayState overlayState = Overlay.of(context);
+    if(overlayEntry==null){ // this is to fix the issue that mousewheel triggers on enter, so it will trigger twice and will not delete the first one, so the first on is stuck
+      overlayEntry = OverlayEntry(
+        builder: (context) => Positioned(
+          top: position.dy,
+          left: position.dx+30,
+          child: Container(
+            decoration: BoxDecoration(
+                border:
+                Border.all(color: Colors.black45, style: BorderStyle.solid)),
+            child: Image.network(
+              analyse.links[0],
+              scale: 2.5,
+            ),
+          ),
+        ),
+      );
+      overlayState.insert(overlayEntry);
+    }
+  }
+
+  deleteOverlay() {
+    if (overlayEntry != null) {
+      overlayEntry.remove();
+      overlayEntry=null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     Analyse analyse = Provider.of<Analysen>(context).analysen[analyseId];
 
-    return Card(
-      shape: RoundedRectangleBorder(
-          side: BorderSide(color: Theme.of(context).accentColor),
-          borderRadius: BorderRadius.circular(10)),
-      child: InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, AnalyseScreen.routeName,
-              arguments: analyse.id);
-        },
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-          child: Row(
-            children: <Widget>[
-              Flexible(
-                flex: 2,
-                child: Center(
+    return /*Listener(
+      onPointerSignal: (PointerSignalEvent event) {
+        if (event is PointerScrollEvent) {
+          print('x: ${event.position.dx}, y: ${event.position.dy}');
+          print('scroll delta: ${event.scrollDelta}');
+        }
+      },
+      child:*/
+    MouseRegion(
+
+      onEnter: (event) {
+        showOverlay(context, analyse, event.position);
+      },
+      onExit: (event) {
+        deleteOverlay();
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+            side: BorderSide(color: Theme.of(context).accentColor),
+            borderRadius: BorderRadius.circular(10)),
+        child: InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, AnalyseScreen.routeName,
+                arguments: analyse.id);
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 2,
+                  child: Center(
                     child: FittedBox(
-                        child:Text(
-                  analyse.title,
-                  style:
-                      TextStyle(fontWeight: FontWeight.bold, fontSize: fSize),
-                )),),
-                fit: FlexFit.tight,
-              ),
-              SizedBox(width: 10,),
-              Flexible(
-                flex: 1,
-                child: Center(
-                    child: FittedBox(
-                      child: Text(
-                  analyse.pair,
-                  style: TextStyle(fontSize: fSize),
-                ),
+                        child: Text(
+                      analyse.title,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: fSize),
                     )),
-                fit: FlexFit.tight,
-              ),
-              SizedBox(width: 10,),
-              Flexible(
-                flex: 1,
-                child: Center(
+                  ),
+                  fit: FlexFit.tight,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Center(
+                      child: FittedBox(
+                    child: Text(
+                      analyse.pair,
+                      style: TextStyle(fontSize: fSize),
+                    ),
+                  )),
+                  fit: FlexFit.tight,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Center(
                     child: FittedBox(
-                        child:Text(
+                        child: Text(
                       "${analyse.date.day}.${analyse.date.month}.${analyse.date.year}",
                       style: TextStyle(fontSize: fSize),
-                    )),),
-                fit: FlexFit.tight,
-              ),
-              SizedBox(width: 10,),
-              Flexible(
-                flex: 3,
-                child: Center(
-                  child: AutoSizeText(
-                    writeTags(analyse.activeTags),
-                    maxLines: 2,
-                    style: TextStyle(color: Colors.blueGrey, fontSize: fSize),
+                    )),
                   ),
+                  fit: FlexFit.tight,
                 ),
-                fit: FlexFit.tight,
-              ),
-            ],
-          ),
-        ),
+                SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  flex: 3,
+                  child: Center(
+                    child: AutoSizeText(
+                      writeTags(analyse.activeTags),
+                      maxLines: 2,
+                      style: TextStyle(color: Colors.blueGrey, fontSize: fSize),
+                    ),
+                  ),
+                  fit: FlexFit.tight,
+                ),
+              ],
+            ),
+          ),),
+        //),
       ),
     );
   }
