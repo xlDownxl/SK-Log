54cd6cce69ae2774e7509a1011a79ee3547bb028
DESKTOP-2ND40HA\nicla <niclas-joswig@gmx.de>
2020-08-27 21:15:27 +0300

    new animation for pairs

--- lib/widgets/entry_list.dart
+++ lib/widgets/entry_list.dart
@@ -19,9 +19,13 @@ class EntryList extends StatefulWidget {
   final bool buildSearchField;
   final Key analysenKey;
   final Key searchFieldKey;
-  EntryList(key, this.buildSearchField, this.analysenKey,
-      this.searchFieldKey,)
-      : super(key: key);
+
+  EntryList(
+    key,
+    this.buildSearchField,
+    this.analysenKey,
+    this.searchFieldKey,
+  ) : super(key: key);
 
   @override
   EntryListState createState() => EntryListState();
@@ -34,13 +38,12 @@ class EntryListState extends State<EntryList> {
   Ascending asc;
   Analysen analysen;
   bool anim;
-  ScrollController scrollController=ScrollController();
+  ScrollController scrollController = ScrollController();
 
   void initState() {
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) {
-          Provider.of<Animations>(context, listen:false).animEntry=false;
-        });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<Animations>(context, listen: false).animEntry = false;
+    });
     super.initState();
   }
 
@@ -48,116 +51,127 @@ class EntryListState extends State<EntryList> {
   Widget build(BuildContext context) {
     asc = Provider.of<Ascending>(context);
     analysen = Provider.of<Analysen>(context);
-    filterMode=Provider.of<FilterMode>(context);
+    filterMode = Provider.of<FilterMode>(context);
     analyseFilter = Provider.of<AnalyseFilter>(context);
-    anim = Provider.of<Animations>(context, listen:false).animEntry;
+    anim = Provider.of<Animations>(context, listen: false).animEntry;
 
-    Widget plusButton=LayoutBuilder(builder: (ctx, constraints) {
+    Widget plusButton = LayoutBuilder(builder: (ctx, constraints) {
       var size = max(constraints.maxWidth, constraints.maxHeight);
       return SizedBox(
-        width: constraints.maxHeight*0.15,
-        height: constraints.maxHeight*0.15,
+        width: constraints.maxHeight * 0.15,
+        height: constraints.maxHeight * 0.15,
         child: FlutterShine(
-          config: Config(shadowColor: Colors.black,),
-      light: Light(intensity: 0.5,),
-      builder:(ctx,ShineShadow shineShadow)=> Material(
-          color: Colors.orange,
-          elevation: 4,
-          shape: CircleBorder(),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(size / 2),
-            child: InkWell(
-              customBorder: CircleBorder(),
-              borderRadius: BorderRadius.circular(25.0),
-              onTap: () {
-                Application.router.navigateTo(context, "/analyse",
-                    transition: TransitionType.fadeIn);
-              },
-              splashColor: Colors.orange[900],
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Center(
-                  child: LayoutBuilder(
-                    builder: (ctx, constr) => Text(
-                        String.fromCharCode(icon.codePoint),
-                        style: TextStyle(
-                            fontSize: constr.maxHeight ,
-                            shadows: shineShadow?.shadows,
-                            fontFamily: icon.fontFamily,
-                            package: icon.fontPackage,
-                            color: Colors.white)),
+          config: Config(
+            shadowColor: Colors.black,
+          ),
+          light: Light(
+            intensity: 0.5,
+          ),
+          builder: (ctx, ShineShadow shineShadow) => Material(
+            color: Colors.orange,
+            elevation: 4,
+            shape: CircleBorder(),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(size / 2),
+              child: InkWell(
+                customBorder: CircleBorder(),
+                borderRadius: BorderRadius.circular(25.0),
+                onTap: () {
+                  Application.router.navigateTo(context, "/analyse",
+                      transition: TransitionType.fadeIn);
+                },
+                splashColor: Colors.orange[900],
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Center(
+                    child: LayoutBuilder(
+                      builder: (ctx, constr) => Text(
+                          String.fromCharCode(icon.codePoint),
+                          style: TextStyle(
+                              fontSize: constr.maxHeight,
+                              shadows: shineShadow?.shadows,
+                              fontFamily: icon.fontFamily,
+                              package: icon.fontPackage,
+                              color: Colors.white)),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-      ),
         ),
       );
     });
 
-    Widget getItem(index){
+    Widget getItem(index) {
       return ListElement(asc.asc
-        ? analysen.analysen.keys.toList()[index]
-        : analysen.analysen.keys.toList().reversed.toList()[index]);
+          ? analysen.analysen.keys.toList()[index]
+          : analysen.analysen.keys.toList().reversed.toList()[index]);
     }
 
     return Stack(
-      children:[
+      children: [
         Container(
-        padding: EdgeInsets.only(left: 50, right: 50, bottom: 15, top: 10),
-        child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Flexible(
-              flex: 2,
+          padding: EdgeInsets.only(left: 50, right: 50, bottom: 15, top: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                flex: 2,
                 child: FilterControl(),
-            ),
-            Headline(),
-            Flexible(
-              flex: 12,
-              child: Container(
-                child:  AnimationLimiter(
-                  child: DraggableScrollbar.rrect(
-                    backgroundColor: Colors.grey,
-                    controller: scrollController,
-                    alwaysVisibleScrollThumb: true,
-                    child: ListView.builder(
-                      padding: EdgeInsets.only(right: 20),
+              ),
+              Headline(),
+              Flexible(
+                flex: 12,
+                child: Container(
+                  child: AnimationLimiter(
+                    child: DraggableScrollbar.rrect(
+                      backgroundColor: Colors.grey,
                       controller: scrollController,
-                      itemBuilder: (ctx, index) {
-                        return anim ?
-                         AnimationConfiguration.staggeredList(
-                          position: index,
-                          duration: const Duration(milliseconds: 500),
-                          child: FlipAnimation(
-                            duration: const Duration(milliseconds: 500),
-                            child: FadeInAnimation(
-                              child: getItem(index),
-                            ),
-                          ),
-                        ): getItem(index);
+                      alwaysVisibleScrollThumb: true,
+                      child: ListView.builder(
+                        padding: EdgeInsets.only(right: 20),
+                        controller: scrollController,
+                        itemBuilder: (ctx, index) {
+                          return anim
+                              ? AnimationConfiguration.staggeredList(
+                                  position: index,
+                                  duration: const Duration(milliseconds: 500),
+                                  child: FlipAnimation(
+                                    duration: const Duration(milliseconds: 500),
+                                    child: FadeInAnimation(
+                                      child: getItem(index),
+                                    ),
+                                  ),
+                                )
+                              : getItem(index);
                         },
-                      itemCount: analysen.analysen.length,
+                        itemCount: analysen.analysen.length,
+                      ),
                     ),
                   ),
                 ),
-              ),),
-          ],
+              ),
+            ],
+          ),
         ),
-      ),
-    Align(
-    alignment: Alignment.bottomRight,
-    child: Container(
-      margin: const EdgeInsets.only(bottom: 10,right: 10,),
-        child: plusButton,
-    ),
-    ),
-        Provider.of<FilterMode>(context).showPairFilter?Container(
-          color: Colors.black.withOpacity(0.7),
-          padding: EdgeInsets.all(50),
-          child: Pairs(),
-        ):Container(),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            margin: const EdgeInsets.only(
+              bottom: 10,
+              right: 10,
+            ),
+            child: plusButton,
+          ),
+        ),
+        Provider.of<FilterMode>(context).showPairFilter
+            ? Container(
+                color: Colors.black.withOpacity(0.7),
+                padding: EdgeInsets.all(50),
+                child: Pairs(),
+              )
+            : Container(),
       ],
     );
   }
--- lib/widgets/entry_list_widgets/pairs.dart
+++ lib/widgets/entry_list_widgets/pairs.dart
@@ -12,14 +12,12 @@ class Pairs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List buildPairs() {
-      var pairs = Provider
-          .of<Analysen>(context, listen: false)
-          .userPairs
-          .getPairs();
+      var pairs =
+          Provider.of<Analysen>(context, listen: false).userPairs.getPairs();
       var pairWidgets = [];
 
       pairs.forEach((pair, value) {
-        pairWidgets.add(GridElement(pair,value));
+        pairWidgets.add(GridElement(pair, value));
       });
       return pairWidgets;
     }
@@ -32,10 +30,12 @@ class Pairs extends StatelessWidget {
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 130,
           childAspectRatio: 2 / 2,
-          crossAxisSpacing: 10,
+          crossAxisSpacing: 5,
           mainAxisSpacing: 10,
         ),
-        children: <Widget>[...buildPairs(),],
+        children: <Widget>[
+          ...buildPairs(),
+        ],
       ),
     );
   }
@@ -44,7 +44,8 @@ class Pairs extends StatelessWidget {
 class GridElement extends StatefulWidget {
   final pair;
   final value;
-  GridElement(this.pair,this.value);
+
+  GridElement(this.pair, this.value);
 
   @override
   _GridElementState createState() => _GridElementState();
@@ -52,10 +53,7 @@ class GridElement extends StatefulWidget {
 
 class _GridElementState extends State<GridElement> {
   var counter = 0;
-  bool colors=false;
-
-  final nonHoverTransform = Matrix4.identity()..translate(0, 0, 0);
-  final hoverTransform = Matrix4.identity()..translate(0, -8, 0);
+  bool colors = false;
   bool _hovering = false;
 
   @override
@@ -67,60 +65,63 @@ class _GridElementState extends State<GridElement> {
       child: ScaleAnimation(
         child: FadeInAnimation(
           child: FlutterShine(
-            config: Config(shadowColor: Theme
-                .of(context)
-                .primaryColor,),
-            light: Light(intensity: 0.5,),
+            config: Config(
+              shadowColor: Theme.of(context).primaryColor,
+            ),
+            light: Light(
+              intensity: 0.5,
+            ),
             builder: (ctx, ShineShadow shineShadow) =>
-                AnimatedContainer(
-                  duration: const Duration(milliseconds: 200),
-                  transform: _hovering ? hoverTransform : nonHoverTransform,
-                  margin: EdgeInsets.only(top: 8),
-                  child: Card(
-                    elevation: 2,
-                    color: !colors ? Colors.orange :  Colors.yellowAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30),
-                      ),
+                 AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.fastOutSlowIn,
+                margin: !_hovering?EdgeInsets.all(8):null,
+
+                child: Card(
+                  elevation: 2,
+                  color: !colors ? Colors.orange : Colors.yellowAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
                     ),
-                    child: InkWell(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        child: Center(
-                          child: FittedBox(
-                            child: Text(
-                              widget.pair,
-                              style:
-                              TextStyle(fontWeight: FontWeight.bold,
-                                fontSize: 30,
-                                //color: Colors.white,
-                                shadows: shineShadow?.shadows,),
+                  ),
+                  child: InkWell(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Center(
+                        child: FittedBox(
+                          child: Text(
+                            widget.pair,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: _hovering?35:30,
+                              //color: Colors.white,
+                              shadows: shineShadow?.shadows,
                             ),
                           ),
                         ),
                       ),
-                      onTap: () {
-                        Provider.of<AnalyseFilter>(context, listen: false)
-                            .addPairFilter(widget.pair);
-                        Provider.of<Analysen>(context, listen: false).setFilter(
-                            Provider.of<AnalyseFilter>(context, listen: false));
-                        Provider.of<FilterMode>(context, listen: false)
-                            .deactivatePairFilter();
-                      },
-                      onHover: (val) {
-                        setState(() {
-                          colors = val;
-                          _hovering = val;
-
-                        });
-                      },
                     ),
+                    onTap: () {
+                      Provider.of<AnalyseFilter>(context, listen: false)
+                          .addPairFilter(widget.pair);
+                      Provider.of<Analysen>(context, listen: false).setFilter(
+                          Provider.of<AnalyseFilter>(context, listen: false));
+                      Provider.of<FilterMode>(context, listen: false)
+                          .deactivatePairFilter();
+                    },
+                    onHover: (val) {
+                      setState(() {
+                        colors = val;
+                        _hovering = val;
+                      });
+                    },
                   ),
                 ),
+              ),
           ),
         ),
       ),
     );
   }
 }
-
